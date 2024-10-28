88ab3e4dc8eeba8b12da27cac52ee0b9907f6f28
Tareun3406 <tareun3406@gmail.com>
2024-10-28 10:58:52 +0900

    initial commit

---
+++ src/main/kotlin/kr/tareun/lumenapi/LumenCalcApiApplication.kt
@@ -0,0 +1,11 @@
+package kr.tareun.lumenapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LumenCalcApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LumenCalcApiApplication>(*args)
+}
---
+++ src/test/kotlin/kr/tareun/lumenapi/LumenCalcApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.tareun.lumenapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LumenCalcApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
